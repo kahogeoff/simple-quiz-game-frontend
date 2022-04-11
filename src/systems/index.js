b0c37ca1c6e2f55d665971a4489590cf5967b18e
Geoffrey Cheung <kahogeoff@gmail.com>
2022-04-12 03:42:42 +0800

    [UpdateFeat] Update the system to adapt the new features and structure

--- src/systems/index.js
+++ src/systems/index.js
@@ -1,9 +1,15 @@
-import { MoveBox } from "./movebox";
 import { UIInitialize } from "./ui_initialize";
-import { UIButtonBehavior } from "./ui_buttobehavior";
+import { UIButtonBehavior } from "./ui_buttonbehavior";
+import { UIUpdate } from "./ui_update";
+import { QuestionInitialize } from "./question_initialize";
+import { GameInitialize } from "./game_initialize";
+import { QuestionAnswering } from "./question_answering";
 
 export default [
+    GameInitialize,
+    QuestionInitialize,
     UIInitialize,
-    UIButtonBehavior
-    // MoveBox
+    UIUpdate,
+    UIButtonBehavior,
+    QuestionAnswering,
 ];
--- src/systems/ui_initialize.js
+++ src/systems/ui_initialize.js
@@ -1,65 +1,68 @@
 import { CounterText, QuizButton, QuestionText } from "../components/ui";
-import GameVar from "../game_var";
 
 const UIInitialize = (entities, args) => {
-    if(!GameVar.initialized){
+    const parameters = entities['game_parameters'];
+    if(!parameters.initialized){
         const btn_color = [
             "blue",
             "red",
             'yellow',
             "green"
         ]
-        GameVar.initialize_view_size = {
+        parameters.initialize_view_size = {
             w: window.innerWidth,
             h: window.innerHeight
         };
 
+        // Initialize the counters
         let correct_counter = { 
             anchor: {
                 r: 100,
                 t: 50
             },
-            text: `Correct: ${GameVar.correct_count}`,
+            text: `Correct: ${parameters.correct_count}`,
             fontSize: 30,
             renderer: <CounterText /> 
         };
-
         let wrong_counter = { 
             anchor: {
                 r: 100,
                 t: 88
             },
-            text: `Wrong: ${GameVar.wrong_count}`,
+            text: `Wrong: ${parameters.wrong_count}`,
             fontSize: 30,
             renderer: <CounterText /> 
         };
         
         entities["ui_counter_correct"] = correct_counter;
         entities["ui_counter_wrong"] = wrong_counter;
+
+        // Initialize the question text
         entities["ui_text_question"] = {
             renderer: <QuestionText />
         };
 
+        // Initialize the question button
         for (let index = 0; index < 4; index++) {
             entities[`ui_btn_opt${index}`] = {
                 id: `ui_btn_opt${index}`,
                 size: {
-                    w: 190 * 1.5,
-                    h: 49 * 1.5
+                    w: 190 * 1.8,
+                    h: 49 * 1.8
                 },
                 fontSize: 20,
                 scale: 1,
                 anchor: {
-                    r: (index % 2 === 1? 120: null),
-                    l: (index % 2 === 1? null: 120),
-                    b: (index < 2? 200: 100)
+                    r: (index % 2 === 1? window.innerWidth * 0.2: null),
+                    l: (index % 2 === 1? null: window.innerWidth * 0.2),
+                    b: (index < 2? 220: 100)
                 },
                 text: `Option ${index+1}`,
                 buttonColor: btn_color[index],
                 renderer: <QuizButton /> 
             };
         }
-        GameVar.initialized = true;
+        parameters.initialized = true;
     }
 
     return entities;
