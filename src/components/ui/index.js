c9121aa0bc4ec1321a9a696beeea37087d381803
Geoffrey Cheung <kahogeoff@gmail.com>
2022-04-12 02:03:53 +0800

    [NewFeat] Add the animated background component for UI export.

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,9 +1,11 @@
 import { CounterText } from "./counter_text";
 import { QuizButton } from "./quiz_button";
 import { QuestionText } from "./question_text";
+import { AnimatedBackground } from "./animated_background";
 
 export {
     CounterText,
     QuizButton,
-    QuestionText
+    QuestionText,
+    AnimatedBackground
 }
